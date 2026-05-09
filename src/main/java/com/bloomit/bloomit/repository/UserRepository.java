cca0c1c34539a8e5a391314a94ec13aa0d679fae
Lidia Sarata <lidiasarata@gmail.com>
2026-05-09 16:59:45 +0200

    #5: Create UserRepository interface

--- src/main/java/com/bloomit/bloomit/repository/UserRepository.java
+++ src/main/java/com/bloomit/bloomit/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.bloomit.bloomit.repository;
 
+import com.bloomit.bloomit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
 
 }
