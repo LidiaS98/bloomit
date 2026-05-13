94dcd4e1adab04b275066d4012bc02252e0f8788
Lidia Sarata <lidiasarata@gmail.com>
2026-05-13 20:21:08 +0200

    #8: Add relationship between User and HabitLog

--- src/main/java/com/bloomit/bloomit/model/HabitLog.java
+++ src/main/java/com/bloomit/bloomit/model/HabitLog.java
@@ -14,12 +14,20 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 
+
+
+
 @Entity
 @Table(name="habit_log")
 public class HabitLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    // JPA relation between User and HabitLog. Many "posts" its assigned to one person with user_id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
 
     @Column(nullable = false)
     int sleepHours;
