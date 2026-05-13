bda93231573b5e30b7be65a5d00478513d217f19
Lidia Sarata <lidiasarata@gmail.com>
2026-05-13 19:28:18 +0200

    #5: Create UserRepository interface - update

--- src/main/java/com/bloomit/bloomit/repository/UserRepository.java
+++ src/main/java/com/bloomit/bloomit/repository/UserRepository.java
@@ -2,8 +2,7 @@ package com.bloomit.bloomit.repository;
 
 import com.bloomit.bloomit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-
+    //by extending JpaRepository I have methods like save(), findById(), findAll(), deleteById() for SQL
 }
