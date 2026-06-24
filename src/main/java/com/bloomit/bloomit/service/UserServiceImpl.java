f4da1a10b7af29c3c8b4d7dcf129f3aaa7766279
Lidia Sarata <lidiasarata@gmail.com>
2026-06-24 16:43:34 +0200

    #24 Add hash to User's passwords

--- src/main/java/com/bloomit/bloomit/service/UserServiceImpl.java
+++ src/main/java/com/bloomit/bloomit/service/UserServiceImpl.java
@@ -3,14 +3,18 @@ package com.bloomit.bloomit.service;
 import com.bloomit.bloomit.model.User;
 import com.bloomit.bloomit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public User createUser(User newUser) {
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
         return userRepository.save(newUser);
     }
 
