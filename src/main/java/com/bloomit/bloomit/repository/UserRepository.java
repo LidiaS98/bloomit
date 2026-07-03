c2e829c459652e8d3005fe686feff97cb1749b6f
Lidia Sarata <lidiasarata@gmail.com>
2026-07-03 17:28:53 +0200

    #32 feat: add method for user's login

--- src/main/java/com/bloomit/bloomit/repository/UserRepository.java
+++ src/main/java/com/bloomit/bloomit/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.bloomit.bloomit.repository;
 import com.bloomit.bloomit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
     //by extending JpaRepository I have methods like save(), findById(), findAll(), deleteById() for SQL
+    Optional<User> findByEmail(String email);
 }
