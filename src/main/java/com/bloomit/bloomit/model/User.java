07cf1ee7e2d4134668ad6adffe31edef8f7de80a
Lidia Sarata <lidiasarata@gmail.com>
2026-07-03 17:54:29 +0200

    #32 add hiding annotation to password, so it won't be visible in JSON

--- src/main/java/com/bloomit/bloomit/model/User.java
+++ src/main/java/com/bloomit/bloomit/model/User.java
@@ -1,5 +1,6 @@
 package com.bloomit.bloomit.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,6 +26,7 @@ public class User {
     @Column(nullable = false, unique = true, length = 254)
     String email;
 
+    @JsonIgnore
     @Column(nullable = false)
     String password;
 
